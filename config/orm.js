052bb63d4fc12efc6b64b20a2620bda0b97220bd
Caroline <caroline.portilla@gmail.com>
2020-05-12 20:23:32 +0800

    working out how to export

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,14 @@
-const connection = require("./config/connection");
+const sqlConnection = require("./connection");
+
+//Selecting all the burgers
+function selectAll(){
+    connection.query("Select * from burgers", function(err,res){
+        if (err) {
+            return res.status(500).end();
+          }
+          return res;
+    })
+}
+
+
+module.exports = Orm;
