c4f6f036a8e8e1e7b92f5497151b572135d57f22
Caroline <caroline.portilla@gmail.com>
2020-05-13 11:40:00 +0800

    server.js done

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const connection = require("./config/connection");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
 
-main();
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const PORT = process.env.PORT || 8080;
 
-function main(){
-    console.log("hi");
-    connection.query("Select * from burgers", function(err,res){
-        console.table(res);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-    })
-}
+  // Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
