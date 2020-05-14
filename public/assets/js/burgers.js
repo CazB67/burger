48cc150a9959834a8f7094e6e91f3f8dfd1f5af3
Caroline <caroline.portilla@gmail.com>
2020-05-14 22:02:44 +0800

    index.html placeholder

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,42 @@
+$(function() {
+    $(".devour-it").on("click", function(event) {
+      let id = $(this).data("id");
+      let eatenBurger = $(this).data("eatenBurger");
+  
+      let newEatenState = {
+        devoured: eatenBurger
+      };
+
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newEatenState
+      }).then(
+        function() {
+          console.log("changed devoured to", eatenBurger);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+    $(".form-control").on("submit", function(event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+    
+        const newBurger = {
+          name: $("#hamburger").val().trim(),
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("created new burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      });
+    });
