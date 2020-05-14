a3cb37540d43b94f487eb5df7baf348eb9a52f80
Caroline <caroline.portilla@gmail.com>
2020-05-14 21:09:12 +0800

    copy pasta from class activity

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,7 @@ INSERT INTO burgers(burger_name, devoured)
 VALUES ("Grilled Chicken", true);
 
 INSERT INTO burgers(burger_name, devoured) 
-VALUES ("Bacon Deluxe", true);
+VALUES ("Bacon Deluxe", false);
 
 INSERT INTO burgers(burger_name, devoured) 
 VALUES ("Beef Supreme", true);
--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const app = express();
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 8081;
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+const routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
   // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
@@ -18,58 +21,3 @@ app.listen(PORT, function() {
   });
 
   
-// Use Handlebars to render the main index.html page with the burgers in it.
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    res.render("index", { burgers: data });
-  });
-});
-
-//Create a new burger
-app.post("/api/burgers", function(req, res) {
-  connection.query("INSERT INTO burgers (burger) VALUES (?)", [req.body.burger], function(err, result) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    // Send back the ID of the new burger
-    res.json({ id: result.insertId });
-    console.log({ id: result.insertId });
-  });
-});
-
-// Update a burger
-app.put("/api/burgers/:id", function(req, res) {
-  connection.query("UPDATE burgers SET burger = ? WHERE id = ?", [req.body.burger, req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-    else if (result.changedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
-
-// Delete a burger
-app.delete("/api/burgers/:id", function(req, res) {
-  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server failure
-      return res.status(500).end();
-    }
-    else if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
