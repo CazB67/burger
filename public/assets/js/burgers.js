97bf904cfa87c1d0e10fa47417954087242c7371
Caroline <caroline.portilla@gmail.com>
2020-05-15 22:31:10 +0800

    preventing multiple clicks and clicks in an empty input box from posting

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,11 +1,10 @@
 $(function() {
     $(".devour-it").on("click", function(event) {
       let id = $(this).data("id");
-      console.log("ID " + id);
       let newEatenState = {
         devoured: 1
       };
-console.log(newEatenState);
+
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newEatenState
@@ -19,11 +18,10 @@ console.log(newEatenState);
 
     $(".throw-up").on("click", function(event) {
       let id = $(this).data("id");
-      console.log("ID " + id);
       let newEatenState = {
         devoured: 0
       };
-console.log(newEatenState);
+
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newEatenState
@@ -36,23 +34,26 @@ console.log(newEatenState);
     });
 
     $(".create-form").on("submit", function(event) {
-        // Make sure to preventDefault on a submit event.
-        event.preventDefault();
+      event.preventDefault();
         let newBurger = {
           burger_name: $("#hamburger").val().trim(),
           devoured: 0
         };
+        
         newBurger.burger_name = newBurger.burger_name.charAt(0).toUpperCase() + newBurger.burger_name.slice(1).toLowerCase(); 
         
-        // Send the POST request.
-        $.ajax("/api/burgers", {
-          type: "POST",
-          data: newBurger
-        }).then(
-          function() {
-            // Reload the page to get the updated list
-            location.reload();
-          }
-        );
+        if(newBurger.burger_name.length > 0){
+          $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+          }).then(
+            function() {
+              // Reload the page to get the updated list
+              location.reload();
+            }
+          );
+        }
       });
     });
+
+    
